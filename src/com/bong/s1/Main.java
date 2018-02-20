dbeb58b60cecce5638e732c2b860ba548c4feef1
ppj1017 <ppj1017@github.com>
2018-02-20 10:23:46 +0900

    Test1
    설명문입니다.

--- src/com/bong/s1/Main.java
+++ src/com/bong/s1/Main.java
@@ -4,9 +4,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		SchoolController schoolController = new SchoolController();
-		
+		System.out.println("프로그램 실행합니다.");
 		schoolController.start();
-		
 
 	}
 
